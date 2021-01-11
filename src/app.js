bdd52c32922b93dc51858da707dcd44f46b750bc
fukiran <macdorphoto@yahoo.com>
2021-01-11 17:03:47 +0000

    add capability to add a new todo to the list

--- src/app.js
+++ src/app.js
@@ -4,7 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el:"#app",
         data: {
-            todos: ["Buy shopping", "Clean bathroom", "Car's MOT"]
+            todos: ["Buy shopping", "Clean bathroom", "Car's MOT"],
+            newItem: ""
+        },
+        methods: {
+            saveNewItem: function (){
+                this.todos.push(this.newItem);
+                this.newItem = "";
+            }
         }
+        
     });
 });
