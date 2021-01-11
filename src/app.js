0f1a6db85113c61162f7447f98efa913a5df9ca0
fukiran <macdorphoto@yahoo.com>
2021-01-11 16:38:35 +0000

    add render a list of todos from an array of strings

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
     new Vue({
-        el:"#app"
+        el:"#app",
+        data: {
+            todos: ["Buy shopping", "Clean bathroom", "Car's MOT"]
+        }
     });
 });
